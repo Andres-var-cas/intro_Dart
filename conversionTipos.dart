79a51bdc9f5844b72a11d59f4e8ca602cc86f043
Andru <andruvargas023@gmail.com>
2025-01-04 00:15:32 -0600

    conversion de tipos

---
+++ conversionTipos.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int edad = 32.3.toInt();
+  print(edad);
+
+  double estatura = double.parse('17.3');
+  print(estatura);
+  
+  String nombre = 42.toString();
+  print(nombre);
+}
