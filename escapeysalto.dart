36d80b9c45aff2bd3e340dfb45dddccea41365c9
Andru <andruvargas023@gmail.com>
2025-01-04 13:29:13 -0600

    escapar caracteres y salto de linea

---
+++ escapeysalto.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int edad = 32;
+  String name = 'Andres';
+  double price = 500;
+
+  // caracter de escape \
+  print('El precio es \$$price');
+  print('\"Hola soy $name\"');
+  print("bienvenido \n a dart");
+  print('''
+  Dart es
+  El mejor
+  Lenguaje 
+  no hay dudas''');
+}
