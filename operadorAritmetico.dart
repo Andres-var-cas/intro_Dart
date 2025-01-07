adebd3da2b5da9895bbe054d5082305603d4b2da
Andru <andruvargas023@gmail.com>
2025-01-07 08:25:57 -0600

    operador aritmetico

---
+++ operadorAritmetico.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a = 20;
+  int b = 10;
+
+  // tipo binario suma
+  print(a + b);
+  // tipo binario resta
+  print(a - b);
+  // tipo binaria multiplicacion
+  print(a * b);
+
+  int c = 30;
+  int d = 14;
+  // tipo binaria division
+  print(c / d);
+  // redondear a entero la division ~
+  print(c ~/ d);
+  // reciduo
+  print(a % b);
+}
