cfacb2a95b2dcb35b36f5a4a4c8b3f0ddd06847d
Andru <andruvargas023@gmail.com>
2025-01-07 08:44:45 -0600

    Ternario

---
+++ operadorTernario.dart
@@ -0,0 +1,9 @@
+void main() {
+  int edad = 15;
+  int edadMinima = 18;
+  print((edad >= edadMinima) ? 'puedes entrar' : 'no puedes entrar');
+
+  // verifica valor null
+  int x = 15;
+  print(x ?? 10);
+}
