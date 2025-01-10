a804e09ff95ecee8d16542b17b9a8b5a58c91d03
Andru <andruvargas023@gmail.com>
2025-01-09 20:22:26 -0600

    mas metodos

--- metodoLista.dart
+++ metodoLista.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  List frutas = ['pera', 'uvas', 'limon'];
+  List frutas = ['pera', 'uvas', 'limon', 'fresas', 'naranja'];
 
   for (var i = 0; i < frutas.length; i++) {
     print(frutas[i]);
@@ -13,4 +13,18 @@ void main(List<String> args) {
   // forEach hace lo mismo sin utilizar mucho codigo
 
   frutas.forEach((var fruta) => print(fruta));
+
+  // sub Lista
+  print(frutas.sublist(1, 4));
+
+  // Aleatorio
+  frutas.shuffle();
+  print(frutas);
+
+  // lista en reversa
+  print(frutas.reversed);
+
+  // cumple con la condicion
+
+  print(frutas.where((var fruta) => fruta == 'limon'));
 }
